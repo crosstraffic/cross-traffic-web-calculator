90f79c0d2458617cb6443fd5c21484be29cbe59d
raynbowy23 <lov.us.basketball@gmail.com>
2022-09-26 23:02:22 -0500

    add front page and modified design

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,16 +2,36 @@
   <title>HCM Auto Calculator</title>
 </svelte:head>
 
-<body class="home">
-  <p>Welcome to HCM Calculator! This site is third-party web site to support calculation along with Highway Capacity Manual (HCM).</p>
+<div class="home grid h-screen place-items-center mt-20">
+  <div class="home_div">
+    <p>Welcome to <b>HCM Calculator</b>! This site is the third-party web site to support calculation along with Highway Capacity Manual (HCM).</p>
+    <p>The HCM Calculator is to provide quick solutions for all of the calculations in HCM.</p>
+    <p>It also illustrates the simple figures for full complement of the understandings.</p>
+
+    <p>This site is launched in November 2022 by <a href="https://reis-portfolio.vercel.app" target="_blank">Rei Tamaru</a>, a PhD student at the University of Wisconsin-Madison.</p>
+
+    <div class="w-48 carousel rounded-box hcmimg" style="float:right">
+      <div class="carousel-item w-full">
+        <img src="hcm7th.jpg" class="w-full" alt="Image of HCM 7th edition" />
+        <div class="middle">
+          <div class="text"><a href="https://nap.nationalacademies.org/catalog/26432/highway-capacity-manual-7th-edition-a-guide-for-multimodal-mobility" target="_blank">+ Buy</a></div>
+        </div>
+        <!-- <caption>Highway Capacity Manual 7th Edition</caption> -->
+      </div> 
+    </div>
+
+    <p>The latest HCM (7th Edition) can be found <a href="https://nap.nationalacademies.org/catalog/26432/highway-capacity-manual-7th-edition-a-guide-for-multimodal-mobility" target="_blank">here (National Academics)</a>.</p>
+
+    <p>For the website inquires, please email <a href="mailto:tamaru@wisc.edu">to me</a>.</p>
+  </div>
   <!-- add update thread and general usage -->
-</body>
+</div>
 
 <footer class="home-footer footer footer-center p-10 bg-base-200 text-base-content rounded">
-  <div class="grid grid-flow-col gap-4">
-    <a class="link link-hover">About us</a> 
+  <!-- <div class="grid grid-flow-col gap-4">
+    <a href="about" class="link link-hover">About</a> 
     <a class="link link-hover">Contact</a> 
-  </div> 
+  </div>  -->
   <div>
     <p>Copyright © 2022 - All right reserved by Rei Tamaru and Jonathan Riel</p>
   </div>
